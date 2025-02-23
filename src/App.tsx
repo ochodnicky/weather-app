f41118e306f0673667559b5939c9018a0e0f8049
Ondrej Ochodnicky <ondrej.ochodnicky@cloudtalk.io>
2025-02-23 16:45:55 +0100

    feat: add macos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {UIManager} from 'react-native';
 
 import {QueryClientProvider} from '@tanstack/react-query';
 import {MD3LightTheme, PaperProvider} from 'react-native-paper';
@@ -7,6 +8,11 @@ import {queryClient} from '@/api/queryClient';
 import {Navigation} from '@/navigation/Navigation';
 
 const App = () => {
+  console.log('Available UI components:', UIManager);
+  console.log(
+    'Has RNSScreenStackHeaderConfig:',
+    UIManager.getViewManagerConfig('RNSScreenStackHeaderConfig'),
+  );
   return (
     <QueryClientProvider client={queryClient}>
       <PaperProvider theme={MD3LightTheme}>
